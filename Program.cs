8c8f95c7b5632c980a3d63f2039bdabbd56b840d
Aca-ac <3467498421@qq.com>
2026-03-11 11:29:27 +0800

    first homework

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好，我是何向玺");
+            string inputLine = Console.ReadLine();
+            string[] inputParts = inputLine.Split(' ');
+            int downlimit = int.Parse(inputParts[0]);
+            int uplimit=int.Parse(inputParts[1]);
+            int count = 0;
+            for (int i = downlimit; i <= uplimit; i++)
+            {
+                bool judge = true;
+                for (int j = 2; j * j <= i; j++)
+                {
+                   if(i%j==0)
+                    {
+                        judge = false;
+                        break;
+                    }
+                }
+                if (judge)
+                {
+                    Console.Write(i+" ");
+                    count++;
+                }
+                if (count == 10)
+                {
+                    Console.WriteLine();
+                    count = 0;
+                }
+            }
+
+        }
+    }
+}
+
